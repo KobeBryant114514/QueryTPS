8c90df1ad661ac705725f09b38aba22ba3deb13b
Tsubasa6848 <116721335+Tsubasa6848@users.noreply.github.com>
2023-03-03 03:24:10 +0800

    Remove useless fuctions

--- src/Main.cpp
+++ src/Main.cpp
@@ -8,26 +8,15 @@
 using namespace std;
 extern Logger logger;
 
-std::string Color(float tps) {
-    auto StrTPS = std::to_string(tps);
-	if (tps < 15 && tps > 10){
-		return "§6" + StrTPS;
+std::string Color(float input, int a, int b) {
+    auto output = std::to_string(input);
+	if (tps < a && tps > b){
+		return "§6" + output;
 	}
-	if (tps <= 10){
-		return "§c" + StrTPS;
+	if (tps <= n){
+		return "§c" + output;
 	}
-	return "§a" + StrTPS;
-}
-
-std::string ColorMSPT(float mspt) {
-    auto Strmspt = std::to_string(mspt);
-	if (mspt >= 50 && mspt <= 100){
-		return "§6" + Strmspt;
-	}
-	if (mspt > 100){
-		return "§c" + Strmspt;
-	}
-	return "§a" + Strmspt;
+	else return "§a" + output;
 }
 
 void RegCommand() {
@@ -36,7 +25,7 @@ void RegCommand() {
         auto command = DynamicCommand::createCommand("tps", "查询服务器TPS", CommandPermissionLevel::Any, { (CommandFlagValue)0x80 }, { (CommandFlagValue)1 });
         command->addOverload();
         command->setCallback([](DynamicCommand const& command, CommandOrigin const& origin, CommandOutput& output, std::unordered_map<std::string, DynamicCommand::Result>& results) {
-            output.success("当前服务器MSPT " + ColorMSPT(mspt) + "，§r相当于实时TPS " + Color(CurrentTPS) + "§r。\n服务器1分钟内平均TPS " + Color(AverageTPS));
+            output.success("当前服务器MSPT " + Color(mspt, 50, 100) + "，§r相当于实时TPS " + Color(CurrentTPS, 15, 10) + "§r。\n服务器1分钟内平均TPS " + Color(AverageTPS, 15, 10));
         });
         DynamicCommand::setup(std::move(command));
 		return true;
